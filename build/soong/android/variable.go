fbfe4387bd630bce7344ade32b45f53fd20b1b4d
Rashed Abdel-Tawab <rashed@linux.com>
2020-04-07 17:15:01 +0200

    pa: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -23,6 +23,9 @@ type Product_variables struct {
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
@@ -33,4 +36,6 @@ type ProductVariables struct {
 	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 }
